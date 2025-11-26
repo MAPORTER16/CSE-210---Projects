e16ee8c9bc177993e218600debb2cad6841d48e8
MAPORTER16 <matthewporter2000@gmail.com>
2025-11-25 17:17:29 -0700

    homework week 5 done

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,9 +1,19 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment a1 = new Assignment("Matthew Porter", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment a2 = new MathAssignment("William Fitzgerald", "Fractions", "7.3", "8-19");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WrittingAssignment a3 = new WrittingAssignment("Kyle Lovato", "European History", "The Causes of World War II");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWrittingInformation());
     }
 }
